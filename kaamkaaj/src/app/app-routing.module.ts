e9374a96df6e90aa91f9314db91d53139269797c
AbdulAhad696 <abdulahad_212@outlook.com>
2022-10-05 15:45:51 +0500

    sign-in/sign-up routing

--- kaamkaaj/src/app/app-routing.module.ts
+++ kaamkaaj/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SignUpComponent } from './sign-up/sign-up.component';
 import { SignInComponent } from './sign-in/sign-in.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
 const routes: Routes = [
   {path:'signup',component:SignUpComponent},
-  {path: 'signin',component:SignInComponent}
+  {path: 'signin',component:SignInComponent},
+  {path:'',component:MainPageComponent}
 ];
 
 @NgModule({
@@ -13,4 +15,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingcomponents=[SignUpComponent,SignInComponent]
+export const routingcomponents=[SignUpComponent,SignInComponent,MainPageComponent]
