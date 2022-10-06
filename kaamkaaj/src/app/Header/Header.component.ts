0cccd4da7f791a3cca25aed5349fe38fb309345e
AbdulAhad696 <abdulahad_212@outlook.com>
2022-10-06 08:47:43 +0500

    header-altered

--- kaamkaaj/src/app/Header/Header.component.ts
+++ kaamkaaj/src/app/Header/Header.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router, Event, NavigationStart, NavigationEnd, NavigationError} from '@angular/router';
 
 @Component({
   selector: 'app-Header',
@@ -11,9 +11,21 @@ export class HeaderComponent implements OnInit {
   adminbuttons=["Complains","Service Providers","Market Statistics","Wallet","Job History","Home"];
   userbuttons=["Home","Service","My Jobs","Contact Admin","Wallet","Add a Job"];
   spbuttons= ["Dashboard","View Jobs","My Projects","Wallet","Contact Admin","Profile"]
-  //loggedin=location.host;
+  mainpagebuttons=["Categories","Community","Contact Us","Find Work", "Find Service"]
+
+  
+  //loggedin:any;
   loggedin="localhost:4200";
-  constructor() { }
+  //login:any
+  constructor(private router:Router) { 
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+          //Hide progress spinner or progress bar
+          //this.loggedin=window.location.href.toString();
+          //console.log("From End: " + this.loggedin);
+      }
+    });
+  }
 
   ngOnInit(): void {
   }
