a2fe06614fb8bd5d3b22e86f8e917afdca4db8a0
Huzaifa-crypton <huzaifa18072002@gmail.com>
2022-10-06 09:19:08 +0500

    Sign in backend integrated with frontend

--- kaamkaaj/src/app/sign-in/sign-in.component.ts
+++ kaamkaaj/src/app/sign-in/sign-in.component.ts
@@ -1,22 +1,29 @@
 import { Component, OnInit } from '@angular/core';
+import { SignInService } from '../Services/sign-in/sign-in.service';
 
 @Component({
   selector: 'app-sign-in',
   templateUrl: './sign-in.component.html',
-  styleUrls: ['./sign-in.component.css']
+  styleUrls: ['./sign-in.component.css'],
+  providers: [SignInService]
 })
 export class SignInComponent implements OnInit {
-  userInput="";
-  passInput="";
-  private username="";
-  private password="";
-  constructor() { }
-  onLogin(){
-    this.username=this.userInput;
-    this.password=this.passInput;
-    this.userInput="";
-    this.passInput="";
-    console.log(this.username,this.password);
+  userInput = "";
+  passInput = "";
+  private username = "";
+  private password = "";
+  constructor(private signinService: SignInService) { }
+  onLogin() {
+    this.username = this.userInput;
+    this.password = this.passInput;
+    this.userInput = "";
+    this.passInput = "";
+    var credentials = {
+      cnicPhoneUsername: this.username,
+      password: this.password
+    }
+    console.log(credentials);
+
     //send request to express API here
   }
   ngOnInit(): void {
