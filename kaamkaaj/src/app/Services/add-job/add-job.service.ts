f66d5d2d4146ec20b69eec2533f87a6407bf613b
KashirSaeed <92178175+KashirSaeed@users.noreply.github.com>
2022-11-17 08:52:56 -0800

    image uploading of adding jobs
    image uploading of adding jobs

--- kaamkaaj/src/app/Services/add-job/add-job.service.ts
+++ kaamkaaj/src/app/Services/add-job/add-job.service.ts
@@ -8,9 +8,9 @@ import { environment } from './../../../environments/environment';
 export class AddJobService {
   constructor(private http:HttpClient) { }
 // ---------------------posting job---------------------------
-  postJobApi(job:any){
-    return this.http.post(environment.baseUrl+"/addJobs" ,job )
-  }
+  // postJobApi(job:any){
+    // return this.http.post(environment.baseUrl+"/addJobs" ,job )
+  // }
 // ------------------------getting specific job object---------------
   getSpecificJob(service:string){
     return this.http.get(`${environment.baseUrl}/services/${service}`)
