67d07e3fe2012a4781207e072cab8a3d2e3d8e0e
Umair Ahmed <umairahmedpaki7@gmail.com>
2022-12-18 10:35:16 +0500

    little changes

--- kaamkaaj/src/app/Services/submitJobByWorker/submit-job-by-worker.service.ts
+++ kaamkaaj/src/app/Services/submitJobByWorker/submit-job-by-worker.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from './../../../environments/environment';
+import { SignInService } from 'src/app/Services/sign-in/sign-in.service';
 
 
 @Injectable({
@@ -8,12 +9,16 @@ import { environment } from './../../../environments/environment';
 })
 export class SubmitJobByWorkerService {
 
-  constructor(private http:HttpClient) { }
+  constructor(private http:HttpClient,private SignInService:SignInService) { }
 
 
   submitJob(id:any,data:any){
+    if(this.SignInService.getusertype()=="Worker"){
 
-    return this.http.patch(`${environment.baseUrl}/serviceProvider/doneProject/${id}`,data)
+      return this.http.patch(`${environment.baseUrl}/serviceProvider/doneProject/${id}`,data)
+    }else{
+      return this.http.patch(`${environment.baseUrl}/client/doneProject/${id}`,data)
+    }
   }
 
 
